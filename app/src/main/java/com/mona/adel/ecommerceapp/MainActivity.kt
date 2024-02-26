264658b0141ad6661bba67f816938858339e57fa
Mona Adel <96449268+MonaAdel2@users.noreply.github.com>
2024-02-26 20:02:56 +0200

    Create splash screen for Android app version 12 and above.

--- app/src/main/java/com/mona/adel/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/mona/adel/ecommerceapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.mona.adel.ecommerceapp
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        initializeSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initializeSplashScreen(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            installSplashScreen()
+            splashScreen.setOnExitAnimationListener {  splashScreenView->
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView, View.TRANSLATION_X, 0f, -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 3000L
+
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                slideUp.start()
+            }
+        }else{
+            setTheme(R.style.Theme_ECommerceApp)
+        }
     }
 }
